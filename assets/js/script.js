6e07be6bdc6e6fdf67d0ab318053522d12236991
Carlos Borrega <c.borrega@proton.me>
2024-01-07 10:21:18 +0000

    created for loop to create multiple time blocks according to every hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,31 +6,35 @@ $('#currentDay').text(today.format("D MMM YYYY"));
 // Timeblocks
 
 var hours = ['8am', '9am', '10am', '11am'];
-var timeBlock = '<div>'
-var textarea = '<textarea>';
-var hour = '<div>';
-var saveBtn = '<button>';
+
+for (var i = 0; i < hours.length; i++) {
+
+    var container = $('.container');
+    var timeBlock = $('<div>').addClass('time-block');
+    var hour = $('<div>').addClass('hour');
+    var textarea = $('<textarea>').addClass('row');
+    var saveBtn = $('<button>').addClass('saveBtn');
+
+    // Append hour, textarea, and save button to the time-block
+    timeBlock.append(hour, textarea, saveBtn);
+
+    // Append the time-block to the container
+    container.append(timeBlock);
+
+}
 
 
-// for (var i = 0; i < hours.length; i++) {
 
-//     $('.container').children().eq(i).addClass('hour time-block');
-//     $('.container').children().eq(i).addClass('row description present');
-// }
+// //Create a new time block and add class
+// container.append(timeBlock);
+// container.children().eq(0).addClass("time-block");
+// var newTimeBlock = $('.time-block');
 
+// //Create the hour/description/button setions of the time block and append
+// newTimeBlock.append(hour);
+// newTimeBlock.append(textarea);
+// newTimeBlock.append(saveBtn);
 
-var container = $('.container');
-
-container.append(timeBlock);
-container.children().eq(0).addClass("time-block");
-var newTimeBlock = $('.time-block');
-
-// container.addClass('row');
-
-newTimeBlock.append(hour);
-newTimeBlock.append(textarea);
-newTimeBlock.append(saveBtn);
-
-newTimeBlock.children().eq(0).addClass('hour');
-newTimeBlock.children().eq(1).addClass('row');
-newTimeBlock.children().eq(2).addClass('saveBtn');
+// newTimeBlock.children().eq(0).addClass('hour');
+// newTimeBlock.children().eq(1).addClass('row');
+// newTimeBlock.children().eq(2).addClass('saveBtn');
